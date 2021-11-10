e7b0b4c67b991366286161f34793ade25f919d14
tonylindorock <54872194+tonylindorock@users.noreply.github.com>
2021-11-10 17:17:59 -0500

    Added street lights

--- tbh/prefabs/maps/map.gd
+++ tbh/prefabs/maps/map.gd
@@ -4,10 +4,17 @@ extends Spatial
 export var music_id = ""
 export var ambience_id = ""
 
+export var weather := -1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	setup()
+	
+
+func setup():
 	play_music()
+	set_weath()
 
 
 func play_music():
@@ -15,3 +22,8 @@ func play_music():
 		SoundManager.play_music(music_id)
 	if ambience_id != "":
 		SoundManager.play_ambience(ambience_id)
+
+
+func set_weath():
+	if weather == 1:
+		$Player/Particles.emitting = true
